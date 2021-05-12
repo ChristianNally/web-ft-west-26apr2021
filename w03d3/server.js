7854e652d6fc40b248244c3e2646654fc89362dc
Christian Nally <sticksallison@gmail.com>
2021-05-12 15:37:45 -0700

    checking in the 'as presented' files for w03d3

--- w03d3/server.js
+++ w03d3/server.js
@@ -4,70 +4,96 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-
 app.set("view engine","ejs");
-
 
 //
 // Users Data
 //
-
 const users = {'nally': "qwerty"};
 
 //
 // Middleware
 //
-
-// app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 //
 // Routes
 //
-
 app.get('/',(req,res)=>{
   res.render('homepage');
 });
 
 // Login Routes
-
 app.get('/login',(req,res)=>{
   res.render("login");
 });
 
 // login submit handler
 app.post("/login",(req,res)=>{
+  const testName = req.body.username;
+  const testPassword = req.body.password;
 
-// ... code goes here.
+  if ( testPassword === users[testName]  ) {
+    res.cookie("user",testName);
+    res.redirect('/profile');
+  } else {
+    res.redirect('/login');
+  }
 
 });
 
 // Registration Routes
-
 app.get('/register',(req,res)=>{
   res.render("register");
 });
 
 // registeration submit handler
 app.post("/register",(req,res)=>{
+  console.log("req.body:",req.body);
 
-// ... code goes here ...
+  console.log("users before:",users);
+  // const users = {'nally': "qwerty"};
+  users[req.body.username] = req.body.password;
+  console.log("users after:",users);
 
+  res.send("Got it!");
+  res.end();
+});
+
+// EDIT
+app.get("/edit",(req,res)=>{
+  const username = req.cookies.user;
+  const password = users[username];
+  res.render("edit", { password });
+});
+
+app.post("/edit", (req,res)=>{
+  console.log("req.body:",req.body);
+
+  const username = req.cookies.user;
+  users[username] = req.body.password;
+  res.send("done writing this part.");
 });
 
 
 // Profile Page
 app.get('/profile',(req,res)=>{
+  console.log("req.cookies:", req.cookies);
 
-  // ... code goes here ...
+  if (users[req.cookies.user]){
+    const secretMessage = "password: " + users[req.cookies.user];
+    res.send(secretMessage);
+  } else {
+    res.redirect('/login');
+  }
 
 });
 
 // Logout Route
 app.get("/logout",(req,res)=>{
-
-  // ... code goes here ...
-
+  res.clearCookie("user");
+  res.redirect('/login');
 });
 
 app.listen(PORT,"localhost", ()=>{
